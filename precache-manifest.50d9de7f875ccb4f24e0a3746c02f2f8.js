7a4e99a04c5130a2160854623a62ff7e630277e7
MichaelPotter1992 <MichaelPotter178@outlook.com>
2022-11-16 05:09:37 -0500

    first commit

---
+++ precache-manifest.50d9de7f875ccb4f24e0a3746c02f2f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1985e8eb676d18c675ceba5d6b1d2d33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cf9ce4749e89a0fc7cf",
+    "url": "/static/css/2.8e2610c1.chunk.css"
+  },
+  {
+    "revision": "2d9c25d60ab2fc8d682d",
+    "url": "/static/css/main.a79213c2.chunk.css"
+  },
+  {
+    "revision": "0cf9ce4749e89a0fc7cf",
+    "url": "/static/js/2.1286914d.chunk.js"
+  },
+  {
+    "revision": "cab22c2f853eb971190951a87268c280",
+    "url": "/static/js/2.1286914d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d9c25d60ab2fc8d682d",
+    "url": "/static/js/main.5dbfdc3d.chunk.js"
+  },
+  {
+    "revision": "79e7a9dc5138f5596451",
+    "url": "/static/js/runtime-main.660e20f8.js"
+  }
+]);
